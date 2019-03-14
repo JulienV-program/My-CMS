ad71744a02e03b2c6c549334d5129d9e15415ed5
JulienV-program <julien.valliorgue@free.fr>
2019-03-14 09:15:42 +0100

    Include Galery

--- public/asset/dropzone.js
+++ public/asset/dropzone.js
@@ -1,3 +1,29 @@
+
+$thumbnail = document.querySelector("#thumbnail");
+
+
+
+$fullSize = document.querySelector("#fullSize");
+
+
+function show($title){
+  console.log($title)
+
+  var ajax = new XMLHttpRequest();
+
+  ajax.onreadystatechange = function(){ $fullSize.innerHTML = '<div id="close" onclick="hide()">Close</div>' +  ajax.response
+    $fullSize.style.display = 'block'}
+
+  ajax.open("GET", "/fullsize/?title="+ $title , true )
+  ajax.send();
+
+}
+
+function hide(){
+  $fullSize.innerHTML = "";
+  $fullSize.style.display = "none";
+}
+
 console.log("hello");
 
 "use strict";
